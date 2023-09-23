f2e43d65076ba30e5ad6157ea1ec92e0745efd66
Vladislav Koldunov <kldn61@gmail.com>
2023-09-23 12:51:22 +0300

    Added Change Ex1

--- Seminar1/EX1/Program.cs
+++ Seminar1/EX1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Возведение в квадрат
+
+Console.Write("Введите число = ");
+string numbersString = Console.ReadLine()!;
+int numbersInt = Convert.ToInt32(numbersString);
+int result = numbersInt * numbersInt;
+Console.WriteLine(result);
