66770c0a3bdc3a463a8d1fefd9f5e430d4def510
Vladislav Koldunov <kldn61@gmail.com>
2023-09-23 14:32:26 +0300

    Added Change Ex2

--- Seminar1/EX2/Program.cs
+++ Seminar1/EX2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Есть два числа, выяснить является одно число квадратом другого
+
+Console.Write("Enter 1st numb1 = ");
+int numb1 = int.Parse(Console.ReadLine());
+Console.Write("Enter 2nd numb2 = ");
+int numb2 = int.Parse(Console.ReadLine());
+
+if(numb1 == numb2 * numb2 || numb2 == numb1 * numb1)
+{
+    Console.WriteLine("true");
+}
+else
+{
+   Console.WriteLine("false"); 
+}
