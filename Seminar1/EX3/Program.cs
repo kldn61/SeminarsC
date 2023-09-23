fdef123841dc0ff58eac525263caaa91e9533644
Vladislav Koldunov <kldn61@gmail.com>
2023-09-23 15:52:36 +0300

    Added Ex3

---
+++ Seminar1/EX3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Написать программу которая будет выдавать дни недели по номеру:
+// 1 - понедельник
+// 5 - пятница
+
+Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine()!);
+if(number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if(number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if(number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if(number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
