e918f99cc2f68ffbfaf2159ca3fbda98577cc0c2
Vladislav Koldunov <kldn61@gmail.com>
2023-09-23 19:15:12 +0300

    Added Ex5

---
+++ Seminar1/EX5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу которая принимает трёхзначное число и выдаёт последнюю цифру этого числа
+// 345 -> 5
+
+Console.Write("Введите число N = ");
+int number = int.Parse(Console.ReadLine()!);
+if(number >= 100 && number < 1000)
+{
+    Console.WriteLine($"Последняя цифра = {number % 10}");
+}
+else
+{
+    Console.WriteLine("Неверно введено число");
+}
