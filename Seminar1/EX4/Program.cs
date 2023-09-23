c479abe60db6236fe863a57bbe6fa845350eeac6
Vladislav Koldunov <kldn61@gmail.com>
2023-09-23 18:57:09 +0300

    Added Change Ex4

--- Seminar1/EX4/Program.cs
+++ Seminar1/EX4/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Написать программу которая принимает число N и выводет все целые числа ]-N; N[
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 
+Console.Write("Enter a number N = ");
+int number = int.Parse(Console.ReadLine()!);
+int negative = -number;
 
+while(negative <= number)
+{
+    Console.WriteLine($"number = {negative}");
+    negative ++;
+}
