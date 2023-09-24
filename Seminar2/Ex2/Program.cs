c291bcaef0df30e789286aea14c885abf337ab42
Vladislav Koldunov <kldn61@gmail.com>
2023-09-24 12:13:00 +0300

    Added Ex2

---
+++ Seminar2/Ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int n = Random.Shared.Next(100, 1000);
+Console.WriteLine(n);
+int lastDigit = n % 10;
+int firstDigit = n / 100;
+int result = firstDigit * 10 + lastDigit;
+Console.WriteLine(firstDigit);
+Console.WriteLine(lastDigit);
+Console.WriteLine(result);
