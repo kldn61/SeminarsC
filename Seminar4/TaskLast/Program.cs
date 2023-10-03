fd36c39c6067d0f1f60bfe8365ba6642e62cdb3f
Vladislav Koldunov <kldn61@gmail.com>
2023-10-04 02:03:13 +0300

    Added Change Sem4

--- Seminar4/TaskLast/Program.cs
+++ Seminar4/TaskLast/Program.cs
@@ -1,33 +1,40 @@
 ﻿// Сложить два массива и получить третий
 
-static void Count(int n);
+internal class Program
 {
-    Random rand = new Random();
-    int[] arr1 = new int[n];
-    int[] arr2 = new int[n];
-    int[] arr3 = new int[n];
-
-    for(int i = 0; i < arr1.Length; i++)
+    private static void Main(string[] args)
     {
-        arr1[i] = rand.Next(0, 100);
-        arr2[i] = rand.Next(0, 100);
-        arr3[i] = arr1[i] + arr2[i];
+        Count(n: 5);
     }
 
-    for(int i = 0; i < arr1.Length; i++)
-    {
-        Console.Write(arr1[i] + " ");
-    }
-    Console.WriteLine();
-
-   for(int i = 0; i < arr1.Length; i++)
-    {
-        Console.Write(arr2[i] + " ");
-    }
-    Console.WriteLine();
-
-    for(int i = 0; i < arr1.Length; i++)
-    {
-        Console.Write(arr3[i] + " ");
-    }
+    
+static void Count (int n)
+{
+Random rand = new Random();
+int[] arr1 = new int[n];
+int[] arr2 = new int[n];
+int[] arr3 = new int[n];
+for (int i = 0; i < arr1.Length; i++)
+{
+arr1[i] = rand.Next(0, 100);
+arr2[i] = rand.Next(0, 100);
+arr3[i] = arr1[i] + arr2[i];
 }
+
+for (int i = 0; i < arr1.Length; i++)
+{
+Console.Write(arr1[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < arr1.Length; i++)
+{
+Console.Write(arr2[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < arr1.Length; i++)
+{
+Console.Write(arr3[i] + " ");
+}
+
+}
+    }
