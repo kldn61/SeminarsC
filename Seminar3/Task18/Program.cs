bb1df82fc49814084c23348a5ee5a95c0274938e
Vladislav Koldunov <kldn61@gmail.com>
2023-10-03 23:03:24 +0300

    Added Seminar3

---
+++ Seminar3/Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N == 1)
+{
+    Console.WriteLine("X > 0, Y > 0");
+}
+else if(N == 2)
+{
+    Console.WriteLine("X < 0, Y > 0");
+}
+else if(N == 3)
+{
+    Console.WriteLine("X < 0, Y < 0");
+}
+else if(N == 4)
+{
+    Console.WriteLine("X > 0, Y < 0");
+}
+else
+{
+    Console.WriteLine("Error");
+}
---
+++ Seminar3/Task20/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух
+// точек и находит расстояние между ними в 2D пространстве.
+// A (3, 6); B (2, 1)  -> 5,09
+// A (7, 5); B (1, 1)  -> 7,21
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = ReadInt("Введите х для точки 1: ");
+int y1 = ReadInt("Введите y для точки 1: ");
+int x2 = ReadInt("Введите х для точки 2: ");
+int y2 = ReadInt("Введите y для точки 2: ");
+
+double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+Console.WriteLine($"Расстояние = {length}");
