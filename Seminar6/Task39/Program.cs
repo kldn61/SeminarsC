1fb56964681d400911c70891983e01550a094e29
Vladislav Koldunov <kldn61@gmail.com>
2023-10-10 01:57:54 +0300

    Added Sem6

---
+++ Seminar6/Task39/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый на последнем и т.д.)
+// [1 2 3 4 5] --> [5 4 3 2 1]
+// [6 7 3 6]   --> [6 3 7 ]
+
+int[] GetRandomArray(int length, int leftSide, int rigthSide)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Random.Shared.Next(leftSide, rigthSide + 1);
+    }
+    return array;
+}
+
+void ReverseArray(int[] sourceArray)
+{
+    for(int i = 0; i < sourceArray.Length / 2; i++)
+    {
+        int temp = sourceArray[i];
+        sourceArray[i] = sourceArray[sourceArray.Length-i-1];
+        sourceArray[sourceArray.Length-i-1] = temp;
+    }
+}
+
+int length = 6;
+int leftSide = -10;
+int rigthSide =10;
+
+int[] myArray = GetRandomArray(length, leftSide, rigthSide);
+Console.WriteLine(string.Join(", ", myArray));
+ReverseArray(myArray);
+Console.WriteLine(string.Join(", ", myArray));
---
+++ Seminar6/Task42/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45  --> 101101
+
+Console.Write("Enter a number = ");
+int number = int.Parse(Console.ReadLine()!);
+
+int Convert(int arg)
+{
+    int arraysize = 0;
+    int number = arg;
+    for (int index = 0; arg > 0; index++)
+    {
+        arg = arg / 2;
+        arraysize++;
+    }
+    return arraysize;
+}
+
+int[] Fillarray(int size, int number)
+{
+    int[] array = new int[size];
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = number % 2;
+        number = number / 2;
+    }
+    return array;
+}
+
+int[] Reversearray(int[] array)
+{
+    for (int index = 0; index < array.Length / 2; index++)
+    {
+        int temp = array[index];
+        array[index] = array[array.Length - 1 - index];
+        array[array.Length - 1 - index] = temp;
+    }
+    return array;
+}
+
+Convert(number);
+int[] array = Fillarray(Convert(number), number);
+Console.WriteLine(string.Join(", ", Reversearray(array)));
