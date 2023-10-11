965929f918357005b0fd74f0ebb119ce3940d500
Vladislav Koldunov <kldn61@gmail.com>
2023-10-11 10:34:50 +0300

    Added Tasks

---
+++ Seminar5/Task34/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 34. Задайте массив заполненный случайными положительными трёхзначными числами Напишите программу,
+// которая покажет количество чётных чисел в массиве
+
+int[] CountEvenElements(int size)
+{
+    int[] array = new int[size];
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("\t ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("\t ");
+    Console.WriteLine();
+}
+
+PrintArray(array);
---
+++ Seminar5/Task37/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5]  --> 5 8 3
+// [6 7 3 6]    --> 12 10
+
+static int[] Arr(int size)
+{
+    Random random = new Random();
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(0, 10);
+    }
+    return arr;
+}
+static int[] NewArr(int[] arr)
+{
+    int[] newArr = new int[arr.Length / 2 + arr.Length % 2];
+    for(int i = 0; i < newArr.Length; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length - 1 - i];
+        if(arr[i] == arr[arr.Length - 1 - i])
+        {
+            newArr[i] = arr[i];
+        }
+    }
+    return newArr;
+}
+
+int size = 10;
+int[] arr = Arr(size);
+Console.Write(String.Join(" ,", NewArr(arr)));
